624b6dd0e8138eac1e2051fd9f650165d4f5cc2c
Vincent Partington <vincent@caspr.io>
2019-10-24 11:33:20 +0200

    Adds test for durable subscriptions
    - Moves utility methods down in the file

--- streaming/nats_streaming_test.go
+++ streaming/nats_streaming_test.go
@@ -18,50 +18,6 @@ import (
 	mutest "github.com/caspr-io/mu-kit/test"
 )
 
-var baseId string
-var lastClientId int64
-
-func init() {
-	kit.Init()
-	zerolog.TimestampFieldName = "t"
-	zerolog.LevelFieldName = "l"
-	zerolog.MessageFieldName = "m"
-
-	rand.Seed(time.Now().UnixNano())
-	baseId = "nats_streaming_test-" + strconv.Itoa(os.Getpid()) + "-"
-	lastClientId = 0
-}
-
-func getNextClientId() string {
-	return baseId + strconv.FormatInt(atomic.AddInt64(&lastClientId, 1), 10)
-}
-
-func getSubject(t *testing.T) string {
-	return baseId + t.Name()
-}
-
-type StanConfig struct {
-	NatsUrl       string `split_words:"true" default:"localhost:4222"`
-	StanClusterId string `split_words:"true"`
-}
-
-func connect(t *testing.T, stanClientId string) stan.Conn {
-	defer mutest.PatchEnvFromFile("../.env")()
-
-	var config StanConfig
-	err := envconfig.Process("", &config)
-	assert.NilError(t, err)
-
-	if config.StanClusterId == "" {
-		t.Skip("STAN_CLUSTER_ID not set, skipping test")
-	}
-
-	sc, err := stan.Connect(config.StanClusterId, stanClientId, stan.NatsURL(config.NatsUrl))
-	assert.NilError(t, err)
-
-	return sc
-}
-
 func TestDoNotReceiveMessagePublishedBeforeSubscribed(t *testing.T) {
 	sc := connect(t, getNextClientId())
 	defer sc.Close()
@@ -96,17 +52,76 @@ func TestDoReceiveMessagePublishedAfterSubscribed(t *testing.T) {
 	assert.Assert(t, receivedMessage)
 }
 
+func TestDoReceiveMessagePublishedBeforeSubscribedDurably(t *testing.T) {
+	sc := connect(t, getNextClientId())
+	defer sc.Close()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	subject := getSubject(t)
+	message := "Hello World"
+
+	defer subscribeAndReceive(t, sc, subject, message, &wg, stan.DurableName(subject+"-durable")).Unsubscribe()
+	publish(t, sc, subject, message)
+
+	receivedMessage := waitWithTimeout(&wg, time.Second*2)
+	assert.Assert(t, receivedMessage)
+}
+
+var baseId string
+var lastClientId int64
+
+func init() {
+	kit.Init()
+	zerolog.TimestampFieldName = "t"
+	zerolog.LevelFieldName = "l"
+	zerolog.MessageFieldName = "m"
+
+	rand.Seed(time.Now().UnixNano())
+	baseId = "nats_streaming_test-" + strconv.Itoa(os.Getpid()) + "-"
+	lastClientId = 0
+}
+
+func getNextClientId() string {
+	return baseId + strconv.FormatInt(atomic.AddInt64(&lastClientId, 1), 10)
+}
+
+func connect(t *testing.T, stanClientId string) stan.Conn {
+	defer mutest.PatchEnvFromFile("../.env")()
+
+	var config struct {
+		NatsUrl       string `split_words:"true" default:"localhost:4222"`
+		StanClusterId string `split_words:"true"`
+	}
+	err := envconfig.Process("", &config)
+	assert.NilError(t, err)
+
+	if config.StanClusterId == "" {
+		t.Skip("STAN_CLUSTER_ID not set, skipping test")
+	}
+
+	sc, err := stan.Connect(config.StanClusterId, stanClientId, stan.NatsURL(config.NatsUrl))
+	assert.NilError(t, err)
+
+	return sc
+}
+
+func getSubject(t *testing.T) string {
+	return baseId + t.Name()
+}
+
 func publish(t *testing.T, sc stan.Conn, subject string, message string) {
 	err := sc.Publish(subject, []byte(message))
 	assert.NilError(t, err)
 }
 
-func subscribeAndReceive(t *testing.T, sc stan.Conn, subject string, message string, wg *sync.WaitGroup) stan.Subscription {
+func subscribeAndReceive(t *testing.T, sc stan.Conn, subject string, message string, wg *sync.WaitGroup, opts ...stan.SubscriptionOption) stan.Subscription {
 	subs, err := sc.Subscribe(subject, func(m *stan.Msg) {
 		if string(m.Data) == message {
 			wg.Done()
 		}
-	})
+	}, opts...)
 	assert.NilError(t, err)
 	return subs
 }
