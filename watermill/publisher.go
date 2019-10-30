4b0e464f4c8351e8d3df3fe8da324b284054e62c
Jeroen van Erp <jeroen@hierynomus.com>
2019-10-30 13:16:55 +0100

    Add name func for publisher

--- watermill/publisher.go
+++ watermill/publisher.go
@@ -1,6 +1,11 @@
 package watermill
 
 import (
+	"reflect"
+
+	"strings"
+	"unicode"
+
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/golang/protobuf/proto"
 	"github.com/satori/uuid"
@@ -27,4 +32,31 @@ func (p *MuPublisher) Publish(msgs ...proto.Message) error {
 	}
 
 	return nil
+}
+
+func DefaultTopicName(m interface{}) string {
+	t := reflect.TypeOf(m)
+
+	name := ""
+	if t.Kind() == reflect.Ptr {
+		name = t.Elem().Name()
+	} else {
+		name = t.Name()
+	}
+
+	var sb strings.Builder
+
+	for i, c := range name {
+		if unicode.IsUpper(c) {
+			if i != 0 {
+				sb.WriteRune('.')
+			}
+
+			sb.WriteRune(unicode.ToLower(c))
+		} else {
+			sb.WriteRune(c)
+		}
+	}
+
+	return sb.String()
 }
---
+++ watermill/publisher_test.go
@@ -0,0 +1,21 @@
+package watermill
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+type TestStruct struct{}
+
+func TestReturnNameOfPointer(t *testing.T) {
+	subject := &TestStruct{}
+
+	assert.Equal(t, DefaultTopicName(subject), "test.struct")
+}
+
+func TestReturnNameOfValue(t *testing.T) {
+	subject := TestStruct{}
+
+	assert.Equal(t, DefaultTopicName(subject), "test.struct")
+}
