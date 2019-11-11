168739f0a40ffa270a6bfee9892860f7c22c6f05
Jeroen van Erp <jeroen@hierynomus.com>
2019-11-11 12:20:49 +0100

    Add testing message handler

--- river/local.go
+++ river/local.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
+	"github.com/golang/protobuf/proto"
 	"github.com/rs/zerolog/log"
 )
 
@@ -23,4 +24,23 @@ func goChannelPubSub(logger watermill.LoggerAdapter) (message.Publisher, message
 	}, logger)
 
 	return pubSub, pubSub
+}
+
+type ChannelMessageHandler struct {
+	Received chan (proto.Message)
+	newMsg   func() proto.Message
+}
+
+func (tmh *ChannelMessageHandler) NewMsg() proto.Message { return tmh.newMsg() }
+func (tmh *ChannelMessageHandler) Name() string          { return "ChannelMessageHandler" }
+func (tmh *ChannelMessageHandler) Handle(ctx *MessageContext, m proto.Message) error {
+	tmh.Received <- m
+	return nil
+}
+
+func NewChannelHandler(f func() proto.Message) *ChannelMessageHandler {
+	return &ChannelMessageHandler{
+		Received: make(chan (proto.Message)),
+		newMsg:   f,
+	}
 }
