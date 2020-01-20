7064bf7974dc4ee40ad6a769d87246c8f40595cd
Vincent Partington <vincent@caspr.io>
2020-01-20 13:46:51 +0100

    Fixes bugs in AddConfigMapVolume and AddEnvironment

--- kubernetes/job.go
+++ kubernetes/job.go
@@ -1,8 +1,6 @@
 package kubernetes
 
 import (
-	"fmt"
-
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -40,34 +38,27 @@ func NewJobSpec(name string, labels map[string]string, containerName string, con
 }
 
 //nolint:gofmt
-func (j *K8sJob) AddConfigMapVolume(cm *v1.ConfigMap, key string, path string, containerName string, mountPath string) {
-	volumeName := fmt.Sprintf("%s-%s", cm.Name, key)
-
+func (j *K8sJob) AddConfigMapVolume(cm *v1.ConfigMap, containerName string, mountPath string) {
 	j.job.Spec.Template.Spec.Volumes = []v1.Volume{
 		v1.Volume{
-			Name: volumeName,
+			Name: cm.Name,
 			VolumeSource: v1.VolumeSource{
 				ConfigMap: &v1.ConfigMapVolumeSource{
 					LocalObjectReference: v1.LocalObjectReference{Name: cm.Name},
-					Items: []v1.KeyToPath{
-						v1.KeyToPath{
-							Key:  key,
-							Path: path,
-						},
-					},
 				},
 			},
 		},
 	}
 
 	vm := v1.VolumeMount{
-		Name:      volumeName,
+		Name:      cm.Name,
 		MountPath: mountPath,
 	}
 
-	for _, c := range j.job.Spec.Template.Spec.Containers {
+	for i, c := range j.job.Spec.Template.Spec.Containers {
 		if c.Name == containerName {
 			c.VolumeMounts = []v1.VolumeMount{vm}
+			j.job.Spec.Template.Spec.Containers[i] = c
 		}
 	}
 }
@@ -78,9 +69,10 @@ func (j *K8sJob) AddEnvironment(containerName string, env map[string]string) {
 		envVars = append(envVars, v1.EnvVar{Name: k, Value: v})
 	}
 
-	for _, c := range j.job.Spec.Template.Spec.Containers {
+	for i, c := range j.job.Spec.Template.Spec.Containers {
 		if c.Name == containerName {
 			c.Env = envVars
+			j.job.Spec.Template.Spec.Containers[i] = c
 		}
 	}
 }
