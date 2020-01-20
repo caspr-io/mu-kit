91d41f9aa47b8afe7de6d3719418225cf0a2c97d
Jeroen van Erp <jeroen@hierynomus.com>
2020-01-20 11:57:00 +0100

    Add more kubernetes glue

--- kubernetes/configmap.go
+++ kubernetes/configmap.go
@@ -5,8 +5,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (k8s *K8s) CreateConfigMap(namespace string, name string, labels map[string]string, key string, payload string) error {
-	_, err := k8s.CoreV1().ConfigMaps(namespace).Create(&v1.ConfigMap{
+func (k8s *K8s) CreateConfigMap(namespace string, name string, labels map[string]string, key string, payload string) (*v1.ConfigMap, error) {
+	cm, err := k8s.CoreV1().ConfigMaps(namespace).Create(&v1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
 			Labels: labels,
@@ -16,7 +16,7 @@ func (k8s *K8s) CreateConfigMap(namespace string, name string, labels map[string
 		},
 	})
 
-	return err
+	return cm, err
 }
 
 func (k8s *K8s) DeleteConfigMap(namespace string, name string) error {
--- kubernetes/job.go
+++ kubernetes/job.go
@@ -1,15 +1,21 @@
 package kubernetes
 
 import (
+	"fmt"
+
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func NewJobSpec(name string, labels map[string]string, containerName string, containerImage string) *batchv1.Job {
-	backoffLimit := int32(0)
+type K8sJob struct {
+	Name string
+	job  *batchv1.Job
+}
 
-	return &batchv1.Job{
+func NewJobSpec(name string, labels map[string]string, containerName string, containerImage string) *K8sJob {
+	backoffLimit := int32(0)
+	j := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
 			Labels: labels,
@@ -29,6 +35,61 @@ func NewJobSpec(name string, labels map[string]string, containerName string, con
 			},
 		},
 	}
+
+	return &K8sJob{Name: name, job: j}
 }
 
-func (k8s *K8s) CreateJob(namespace string, jobSpec *batchv1.JobSpec) {}
+//nolint:gofmt
+func (j *K8sJob) AddConfigMapVolume(cm *v1.ConfigMap, key string, path string, containerName string, mountPath string) {
+	volumeName := fmt.Sprintf("%s-%s", cm.Name, key)
+
+	j.job.Spec.Template.Spec.Volumes = []v1.Volume{
+		v1.Volume{
+			Name: volumeName,
+			VolumeSource: v1.VolumeSource{
+				ConfigMap: &v1.ConfigMapVolumeSource{
+					LocalObjectReference: v1.LocalObjectReference{Name: cm.Name},
+					Items: []v1.KeyToPath{
+						v1.KeyToPath{
+							Key:  key,
+							Path: path,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	vm := v1.VolumeMount{
+		Name:      volumeName,
+		MountPath: mountPath,
+	}
+
+	for _, c := range j.job.Spec.Template.Spec.Containers {
+		if c.Name == containerName {
+			c.VolumeMounts = []v1.VolumeMount{vm}
+		}
+	}
+}
+
+func (j *K8sJob) AddEnvironment(containerName string, env map[string]string) {
+	envVars := []v1.EnvVar{}
+	for k, v := range env {
+		envVars = append(envVars, v1.EnvVar{Name: k, Value: v})
+	}
+
+	for _, c := range j.job.Spec.Template.Spec.Containers {
+		if c.Name == containerName {
+			c.Env = envVars
+		}
+	}
+}
+
+func (j *K8sJob) AddPullSecret(secretName string) {
+	j.job.Spec.Template.Spec.ImagePullSecrets = []v1.LocalObjectReference{{Name: secretName}}
+}
+
+func (k8s *K8s) CreateJob(namespace string, jobSpec *K8sJob) error {
+	_, err := k8s.BatchV1().Jobs(namespace).Create(jobSpec.job)
+	return err
+}
