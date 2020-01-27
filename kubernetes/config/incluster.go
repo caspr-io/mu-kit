9841e3e01795cd6587d0e34c30111234a05ed051
Jeroen van Erp <jeroen@hierynomus.com>
2020-01-27 10:58:05 +0100

    Change log level and collect all fallback errors

--- kubernetes/config/incluster.go
+++ kubernetes/config/incluster.go
@@ -12,10 +12,12 @@ type InClusterConfigLoader struct {
 func (l *InClusterConfigLoader) Load() (*rest.Config, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Info().
+		log.Debug().
 			Err(err).
 			Msg("Cannot load in-cluster k8s configuration, falling back")
+
 		return fallback(err, l.Fallback)
 	}
+
 	return config, nil
 }
--- kubernetes/config/loader.go
+++ kubernetes/config/loader.go
@@ -2,14 +2,25 @@ package config
 
 import "k8s.io/client-go/rest"
 
+import "github.com/caspr-io/mu-kit/util"
+
 type Loader interface {
 	Load() (*rest.Config, error)
 }
 
 func fallback(err error, loader Loader) (*rest.Config, error) {
+	collector := &util.ErrorCollector{}
+	collector.Collect(err)
+
 	if loader != nil {
-		return loader.Load()
-	} else {
-		return nil, err
+		c, e := loader.Load()
+		if e != nil {
+			collector.Collect(e)
+			return nil, collector
+		}
+
+		return c, nil
 	}
+
+	return nil, collector
 }
