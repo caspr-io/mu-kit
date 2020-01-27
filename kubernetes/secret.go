46b96fc9d9bd5e20e7849a27081ac10fec7c6ca7
Jeroen van Erp <jeroen@hierynomus.com>
2020-01-27 23:44:02 +0100

    Change createSecret return type

--- kubernetes/secret.go
+++ kubernetes/secret.go
@@ -5,8 +5,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (k8s *K8s) CreateSecret(namespace string, name string, labels map[string]string, key string, payload string) error {
-	_, err := k8s.CoreV1().Secrets(namespace).Create(&v1.Secret{
+func (k8s *K8s) CreateSecret(namespace string, name string, labels map[string]string, key string, payload string) (*v1.Secret, error) {
+	return k8s.CoreV1().Secrets(namespace).Create(&v1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
 			Labels: labels,
@@ -15,8 +15,6 @@ func (k8s *K8s) CreateSecret(namespace string, name string, labels map[string]st
 			key: payload,
 		},
 	})
-
-	return err
 }
 
 func (k8s *K8s) DeleteSecret(namespace string, name string) error {
